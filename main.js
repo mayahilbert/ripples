0abf6c684208d2e61f41f6892f5d84297bbc0219
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2024-06-13 14:33:54 -0400

    testing

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('anim'),
-    render: 'canvas',
-    loop: true,
-    autoplay: true,
-    path: 'data.json'
-})
+var projectTitleRippled = 0;
+var projectTitleGif;
+var projectTitleStatic;
+        $(function () {
+            $("#project-title-ripple").hover(
+                function () {
+                    if (projectTitleRippled == 0) {
+                        $(this).attr("src", projectTitleGif);
+                        projectTitleRippled++;
+                    }
+                    setTimeout(() => {
+                        $(this).attr("src", projectTitleStatic);
+                        projectTitleRippled = 0;
+                    }, 10000);
+                }
+            );
+            $("#project-title-ripple").click(
+                function () {
+                    $(this).attr("src", projectTitleStatic);
+                    projectTitleRippled = 0;
+                }
+            );
+        });
