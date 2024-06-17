f06c694541aa7c7fa3228664fa626b83438db5ff
angelabernabeo <angela.bernabeo@baruchmail.cuny.edu>
2024-06-17 12:51:15 -0400

    show raindrop, hide/display main content

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ var projectTitleStatic;
                 }
             );
         });
+
+        function setupVideo () {
+            // get video
+              var video = document.getElementById('myVideo')
+              // Play the video, this is optional
+              video.play();
+              // Add a listener to this video, so that when the video ends, the video is "hidden".
+              video.addEventListener('ended', function() {
+              // hide video
+                video.style.display = "none";
+              })
+            }
+            document.addEventListener('DOMContentLoaded', e => {
+                var video = document.getElementById("myVideo");
+                var main = document.getElementById("main-content");
+              
+                video.addEventListener('ended', e => {
+                  main.style.display = "block";
+                  video.style.display = "none";
+                });
+              });
